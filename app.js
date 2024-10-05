1cd5a1ba00d0313b81d64ebef26f1f00b371d3eb
DaveT86 <davetaylor1205@gmail.com>
2024-10-05 22:39:05 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('roulette-button').addEventListener('click', function() {
+    window.location.href = 'roulette/roulette.html'; // Redirect to Roulette page
+});
+
+document.getElementById('blackjack-button').addEventListener('click', function() {
+    window.location.href = 'blackjack.html'; // Redirect to Blackjack page
+});
+
+document.getElementById('punto-banco-button').addEventListener('click', function() {
+    window.location.href = 'punto-banco.html'; // Redirect to Punto Banco page
+});
