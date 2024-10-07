df7876650f318093da6be516118712d80edb08ee
DaveT86 <davetaylor1205@gmail.com>
2024-10-07 05:37:20 +0100

    v1.1

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,10 +2,6 @@ document.getElementById('roulette-button').addEventListener('click', function() 
     window.location.href = 'roulette/roulette.html'; // Redirect to Roulette page
 });
 
-document.getElementById('blackjack-button').addEventListener('click', function() {
-    window.location.href = 'blackjack.html'; // Redirect to Blackjack page
-});
-
 document.getElementById('punto-banco-button').addEventListener('click', function() {
     window.location.href = 'punto/punto.html'; // Redirect to Punto Banco page
 });
